a6bbd4cfecdecd1faa5dd26c4c5d36e5d8ee08f4
Robert Gleason <l2ob122@gmail.com>
2025-12-10 15:30:01 -0800

    day 1, working with variables

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+print(city)
+pet_name = input("What is the name of your pet?\n")
+print(pet_name)
+print(f"Your band name could be {city} {pet_name}")
